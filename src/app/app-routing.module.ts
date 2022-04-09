92519a341ac39414714a4d733f19d2813e56ffaa
clennam <amkahandawala@gmail.com>
2022-04-10 01:20:14 +1000

    secure routes with authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './core/auth/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { SearchResultsComponent } from './pages/search-results/search-results.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/search', pathMatch: 'full' },
-  { path: 'search', component: SearchPageComponent },
-  { path: 'results', component: SearchResultsComponent },
-  { path: 'results/:searchTerm', component: SearchResultsComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'search', component: SearchPageComponent, canActivate: [AuthGuard] },
+  { path: 'results', component: SearchResultsComponent, canActivate: [AuthGuard] },
+  { path: 'results/:searchTerm', component: SearchResultsComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
